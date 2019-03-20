3e5821f15dba6dced40f38de271cc5ab84b4c3ab
Md Rashidul Islam <rashidul0405@gmail.com>
2019-03-20 19:11:11 +0600

    Final version 1

---
+++ _3_thinking_in_react/code/final/src/components/Button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = ({enabled}) => {
+  return (
+    <button type="button" className={`btn btn-outline-${enabled ? "primary" : "success"}`}>Book Now</button>
+  )
+}
+
+export default Button
