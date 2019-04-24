7399ef091bec54beeb37bb03e0af12662080b942
Md Rashidul Islam <rashidul0405@gmail.com>
2019-04-24 12:27:26 +0600

    add a bootstrap switch

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -4,10 +4,23 @@ import Card from "./components/Card"
 import items from "./data"
 
 const App = () => {
-  console.log(items)
   return (
     <div className="container">
       {/*JUMBO TEXT*/}
+      <div className="row align-self-end">
+        <div className="col-auto mr-auto" />
+        <div className="col-auto">
+          <button
+            type="button"
+            className="btn btn-toggle mr-0 mb-1 mt-1 active"
+            data-toggle="button"
+            aria-pressed="false"
+            autoComplete="off"
+          >
+            <div className="handle" />
+          </button>
+        </div>
+      </div>
       <div className="row">
         <div className="jumbotron">
           <h1 className="display-4">Thinking in React!</h1>
