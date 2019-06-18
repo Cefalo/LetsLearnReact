6a54c224fa4fcb39f82d1a4cc0cb71e411ceb386
kawnayeen <kawnayeen.08@gmail.com>
2019-06-18 20:49:02 +0600

    content taken to Home component

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -1,31 +1,8 @@
 import React from 'react'
-import Search from './components/Search'
-import Card from './components/Card'
-import items from "./data"
+import Home from './components/Home';
 
 const App = () => {
-  console.log(items)
-  return (
-    <div className="container">
-      {/*JUMBO TEXT*/}
-      <div className="row">
-        <div className="jumbotron">
-          <h1 className="display-4">Thinking in React!</h1>
-          <p className="lead">React makes it painless to create interactive UIs. Design simple views for each state in
-            your application, and React will efficiently update and render just the right components when your data
-            changes.</p>
-        </div>
-      </div>
-      {/*Search*/}
-      <Search/>
-      <hr className="my-4"/>
-      <div className="row">
-        {items.map((item, i) => <Card key={i} item={item}/>)}
-      </div>
-
-
-    </div>
-  )
-}
+    return <Home/>
+};
 
 export default App
