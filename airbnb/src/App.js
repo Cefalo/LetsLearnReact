c3cc89d1c04ae183fbaca35ef2bfa732ac91c805
kawnayeen <kawnayeen.08@gmail.com>
2019-06-18 22:06:19 +0600

    moving down shortest path to load expected component

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -10,9 +10,9 @@ const App = () => {
             <NavBar/>
             <BrowserRouter>
                 <Switch>
-                    <Route path="/" component={Home}/>
                     <Route path="/home" component={Home}/>
                     <Route path="/about" component={About}/>
+                    <Route path="/" component={Home}/>
                 </Switch>
             </BrowserRouter>
         </React.Fragment>
