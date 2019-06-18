3d1baa40dbe1b41661a279cf17e6ae61a4974468
kawnayeen <kawnayeen.08@gmail.com>
2019-06-18 22:04:35 +0600

    wrapping route in 'Switch' component, solved loading multiple component

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -2,16 +2,18 @@ import React from 'react'
 import Home from './components/Home';
 import About from './components/About';
 import NavBar from './components/NavBar';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 const App = () => {
     return (
         <React.Fragment>
             <NavBar/>
             <BrowserRouter>
-                <Route path="/" component={Home}/>
-                <Route path="/home" component={Home}/>
-                <Route path="/about" component={About}/>
+                <Switch>
+                    <Route path="/" component={Home}/>
+                    <Route path="/home" component={Home}/>
+                    <Route path="/about" component={About}/>
+                </Switch>
             </BrowserRouter>
         </React.Fragment>
     );
