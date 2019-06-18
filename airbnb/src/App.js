37e96f0ca7d0558c7ded516b2fb231f700fe4571
kawnayeen <kawnayeen.08@gmail.com>
2019-06-18 21:29:42 +0600

    very basic brower route added

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -2,12 +2,16 @@ import React from 'react'
 import Home from './components/Home';
 import About from './components/About';
 import NavBar from './components/NavBar';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 const App = () => {
     return (
         <React.Fragment>
             <NavBar/>
-            <Home/>
+            <BrowserRouter>
+                <Route path="/home" component={Home}/>
+                <Route path="/about" component={About}/>
+            </BrowserRouter>
         </React.Fragment>
     );
 };
