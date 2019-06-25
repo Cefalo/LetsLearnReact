6c9e6e635f318fdfa203a9a185b6b8ee37e83df2
kawnayeen <kawnayeen.08@gmail.com>
2019-06-25 22:43:22 +0600

    integrating redux with react app

--- airbnb/src/index.js
+++ airbnb/src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'
+import App from './App';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducers from './reducers';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const store = createStore(reducers);
+
+const app = (
+    <Provider store={store}>
+        <App/>
+    </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
