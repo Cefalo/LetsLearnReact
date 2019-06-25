ee9a97194ae6a4a34561bdb166001f1a02ee71e3
kawnayeen <kawnayeen.08@gmail.com>
2019-06-25 23:06:52 +0600

    implementing the logic of 'apartment reducer'

--- airbnb/src/reducers/apartment.reducer.js
+++ airbnb/src/reducers/apartment.reducer.js
@@ -1,3 +1,10 @@
-export default (state, action) => {
-    return null;
+import {FETCH_APARTMENT} from '../actions';
+
+export default (state = [], action) => {
+    switch (action.type) {
+        case FETCH_APARTMENT:
+            return action.payload;
+        default:
+            return state;
+    }
 }
