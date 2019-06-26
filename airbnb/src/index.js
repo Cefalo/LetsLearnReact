358a92da2b695fdea472c2645436585009746228
Kamarul Kawnayeen <kawnayeen.08@gmail.com>
2019-06-26 10:02:28 +0600

    installation and integration of 'redux-thunk' middleware

--- airbnb/src/index.js
+++ airbnb/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import reducers from './reducers';
+import thunk from 'redux-thunk';
 
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(thunk));
 
 const app = (
     <Provider store={store}>
