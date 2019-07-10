36166961bac7703a98576340f0f7feb4ae791d0f
Md Rashidul Islam <rashidul0405@gmail.com>
2019-07-10 18:00:24 +0600

    convert class components to hooks

--- airbnb/src/components/Home.js
+++ airbnb/src/components/Home.js
@@ -1,44 +1,44 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Search from './Search';
 import Card from './Card';
 import PropTypes from 'prop-types';
-import {connect} from "react-redux";
-import {fetchApartment, fetchFromGithub, searchApartments} from '../actions';
+import {useSelector, useDispatch} from "react-redux";
+import {fetchFromGithub, searchApartments} from '../actions';
 
-class Home extends React.Component {
+const Home = () => {
+    const apartments = useSelector(state => state.searchedApartment)
+    const dispatch = useDispatch()
 
-    componentDidMount() {
-        this.props.fetchFromGithub();
+    useEffect( () => {
+        dispatch(fetchFromGithub())
+    }, [])
+
+    const searchLocation = (location) => {
+        console.log(`At home, location: ${location}`)
+        dispatch(searchApartments(location))
     }
 
-    searchLocation(location){
-        console.log(`At home, location: ${location}`);
-        this.props.searchApartments(location);
-    }
-
-    render() {
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="jumbotron">
-                        <h1 className="display-4">Thinking in React!</h1>
-                        <p className="lead">React makes it painless to create interactive UIs. Design simple views for
-                            each
-                            state in
-                            your application, and React will efficiently update and render just the right components
-                            when
-                            your data
-                            changes.</p>
-                    </div>
-                </div>
-                <Search onSearch={location => this.searchLocation(location)}/>
-                <hr className="my-4"/>
-                <div className="row">
-                    {this.props.apartments.map((item, i) => <Card key={i} item={item}/>)}
-                </div>
-            </div>
-        );
-    }
+    return (
+      <div className="container">
+          <div className="row">
+              <div className="jumbotron">
+                  <h1 className="display-4">Thinking in React!</h1>
+                  <p className="lead">React makes it painless to create interactive UIs. Design simple views for
+                      each
+                      state in
+                      your application, and React will efficiently update and render just the right components
+                      when
+                      your data
+                      changes.</p>
+              </div>
+          </div>
+          <Search onSearch={location => searchLocation(location)}/>
+          <hr className="my-4"/>
+          <div className="row">
+              {apartments.map((item, i) => <Card key={i} item={item}/>)}
+          </div>
+      </div>
+    )
 }
 
 Home.propTypes = {
@@ -48,14 +48,15 @@ Home.propTypes = {
     searchApartments: PropTypes.func
 };
 
-const mapStateToProps = state => {
-    return {
-        apartments: state.searchedApartment
-    }
-};
-
-export default connect(mapStateToProps, {
-    fetchApartment,
-    fetchFromGithub,
-    searchApartments
-})(Home);
+// const mapStateToProps = state => {
+//     return {
+//         apartments: state.searchedApartment
+//     }
+// };
+//
+// export default connect(mapStateToProps, {
+//     fetchApartment,
+//     fetchFromGithub,
+//     searchApartments
+// })(Home);
+export default Home
--- airbnb/src/index.js
+++ airbnb/src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux';
 import reducers from './reducers';
 import thunk from 'redux-thunk';
+import { createLogger } from "redux-logger"
 
-const store = createStore(reducers, applyMiddleware(thunk));
+const composeEnhancers =
+  (process.env.NODE_ENV !== "production" &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose
+
+const middlewares = [thunk]
+
+if (process.env.NODE_ENV !== "production") {
+  middlewares.push(createLogger())
+}
+
+const store = createStore(reducers, composeEnhancers(applyMiddleware(...middlewares)));
 
 const app = (
     <Provider store={store}>
